3c0e6b9eec262786c28253eaddca766526936949
Mersudin Murić <100204090+MeskoCodes@users.noreply.github.com>
2025-01-03 18:23:02 +0100

    Added services, Implemented CRUD operations for each entity with proper API integration. Updated API endpoints and service logic for handling requests and responses efficiently.

--- Services/Services/TreatmentService/ITreatmentService.cs
+++ Services/Services/TreatmentService/ITreatmentService.cs
@@ -7,7 +7,7 @@ namespace Services
         Task<GeneralResponseDto> Create(TreatmentCreateDto treatmentDto, CancellationToken cancellationToken = default);
         Task<GeneralResponseDto> Delete(int treatmentId, CancellationToken cancellationToken = default);
         Task<GeneralResponseDto> Update(int treatmentId, TreatmentUpdateDto treatmentDto, CancellationToken cancellationToken = default);
-        Task<ObservableCollection<TreatmentDto>> GetAll(CancellationToken cancellationToken = default);
+        Task<ObservableCollection<TreatmentDto>> GetAll(CancellationToken cancellationToken = default); 
         Task<TreatmentDto> GetById(int treatmentId, CancellationToken cancellationToken = default);
     }
 }
